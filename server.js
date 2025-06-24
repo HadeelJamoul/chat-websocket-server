63edef25a6a10f9dbcd2b24bed368caafc7da2c0
HadeelJamoul <hadeelgamol@gmail.com>
2025-06-24 13:53:08 +0300

    Initial WebSocket server

---
+++ server.js
@@ -0,0 +1,24 @@
+const WebSocket = require('ws');
+const PORT = process.env.PORT || 10000
+
+const server = new WebSocket.Server({ port: PORT });
+
+let clients = [];
+
+server.on('connection', (ws) => {
+    clients.push(ws);
+
+    ws.on('message', (message) => {
+        clients.forEach(client => {
+            if(client !== ws && client.readyState === WebSocket.OPEN){
+                client.send(message);
+            }
+        })
+    });
+
+    ws.on('close', () => {
+        clients = clients.filter(client => client )
+    })
+});
+
+console.log(`✅ WebSocket server running on port ${PORT}`);
